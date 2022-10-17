5cc2c9563497b150cae714b62f7a53d94a9db8f0
jamshed <jamshed@umd.edu>
2022-10-17 15:33:49 -0400

    Use obj-pool for producer-buffers

--- include/Key_Value_Collator.hpp
+++ include/Key_Value_Collator.hpp
@@ -14,6 +14,7 @@
 #include <utility>
 #include <fstream>
 #include <cstdlib>
+#include <cstdio>
 #include <iostream>
 #include <thread>
 #include <cassert>
@@ -23,6 +24,9 @@
 
 namespace key_value_collator
 {
+
+
+template <typename T_obj_> class Object_Pool;
 
 // A class to: collate a collection of key-value pairs, deposited from multiple
 // producers; and to iterate over the collated key-value collection. Keys are of
@@ -46,12 +50,15 @@ private:
     static constexpr std::size_t partition_buf_mem = (1LU * 1024 * 1024);   // Maximum memory for a partition buffer: 1MB.
     static constexpr std::size_t partition_buf_elem_th = partition_buf_mem / sizeof(key_val_pair_t);    // Maximum number of pairs to keep in a partition buffer.
 
-    std::vector<std::vector<key_val_pair_t>> partition_buf; // `partition_buf[i]` is the in-memory buffer for partition `i`.
+    typedef std::vector<key_val_pair_t> buf_t;  // Type of the data buffers.
+    std::vector<buf_t> partition_buf;   // `partition_buf[i]` is the in-memory buffer for partition `i`.
     std::vector<std::ofstream> partition_file;  // `partition_file[i]` is the disk-storage file for partition `i`.
 
-    moodycamel::ConcurrentQueue<std::vector<key_val_pair_t>*> buf_q;    // Buffer spaces to copy-in incoming data from the producers.
-    std::atomic<uint64_t> produced_buf; // Number of buffers deposited by the producers.
-    std::atomic<uint64_t> consumed_buf; // Number of buffers processed by the collator.
+    Object_Pool<buf_t*> free_buf_pool;  // Buffers to copy-in incoming data from the producers.
+    Object_Pool<buf_t*> full_buf_pool;  // Deposited data from the producers.
+
+    const std::size_t buf_count;    // Number of concurrent buffers for the producers.
+    static constexpr std::size_t buf_count_default = 16;    // Default value for the concurrent buffer count.
 
     std::thread* mapper;    // The background thread mapping key-value pairs to corresponding partitions.
     std::atomic<bool> stream_incoming;  // Flag denoting whether the incoming key-value streams have ended or not.
@@ -76,11 +83,6 @@ private:
     // clears the buffer.
     void flush(std::size_t partition_id);
 
-    // Closes the deposit stream incoming from the producers and flushes the
-    // remaining in-memory content to disk. All deposit operations from the
-    // producers must be made before invoking this.
-    void close_deposit_stream();
-
 
 public:
 
@@ -88,13 +90,21 @@ public:
     // Constructs a key-value pair collection object to collate (and iterate
     // over) key-value pairs produced from multiple producers. Temporary disk-
     // files used throughout the process will be stored at the path-prefix
-    // `work_file_pref`.
-    Key_Value_Collator(const std::string& work_file_pref = work_file_pref_default);
+    // `work_file_pref`. `buf_count` concurrent buffers would be used to store
+    // and process the deposited data. It should be set to at least the number
+    // of producers to avoid throttling of the producers; and a good heuristic
+    // choice for this is twice the number of producers.
+    Key_Value_Collator(const std::string& work_file_pref = work_file_pref_default, std::size_t buf_count = buf_count_default);
 
     ~Key_Value_Collator();
 
     // Deposits the buffer content of `buf` to the collator.
     void deposit(const std::vector<key_val_pair_t>& buf);
+
+    // Closes the deposit stream incoming from the producers and flushes the
+    // remaining in-memory content to disk. All deposit operations from the
+    // producers must be made before invoking this.
+    void close_deposit_stream();
 };
 
 
@@ -131,6 +141,10 @@ public:
     bool empty() const { return size_ == 0; }
 
 
+    // Returns the size of the pool.
+    std::size_t size() const { return size_; }
+
+
     // Tries to fetch an object to `obj` from the pool. Returns `true` iff
     // such an object is found.
     bool fetch(T_obj_& obj)
@@ -159,11 +173,12 @@ public:
 
 
 template <typename T_key_, typename T_val_, typename T_hasher_>
-inline Key_Value_Collator<T_key_, T_val_, T_hasher_>::Key_Value_Collator(const std::string& work_file_pref):
+inline Key_Value_Collator<T_key_, T_val_, T_hasher_>::Key_Value_Collator(const std::string& work_file_pref, const std::size_t buf_count):
     hash(),
     work_file_pref(work_file_pref),
     partition_buf(partition_count),
     partition_file(partition_count),
+    buf_count(buf_count),
     mapper(nullptr),
     stream_incoming(true)
 {
@@ -173,17 +188,45 @@ inline Key_Value_Collator<T_key_, T_val_, T_hasher_>::Key_Value_Collator(const s
     {
         partition_buf[p_id].reserve(partition_buf_elem_th);
         partition_file[p_id].open(partition_file_path(p_id), std::ios::out | std::ios::binary);
-
-        mapper = new std::thread(&Key_Value_Collator<T_key_, T_val_, T_hasher_>::map, this);
     }
+
+    for(std::size_t i = 0; i < buf_count; ++i)
+        free_buf_pool.push(new buf_t());
+
+    mapper = new std::thread(&Key_Value_Collator<T_key_, T_val_, T_hasher_>::map, this);
 }
 
 
 template <typename T_key_, typename T_val_, typename T_hasher_>
 inline Key_Value_Collator<T_key_, T_val_, T_hasher_>::~Key_Value_Collator()
 {
+    if(!full_buf_pool.empty() || free_buf_pool.size() != buf_count || mapper->joinable())
+    {
+        std::cerr << "Collator destructed while unprocessed buffers remained. Aborting.\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+
+    // Release memory of the partition-buffers.
+    buf_t* buf_p = nullptr;
+    while(!free_buf_pool.empty())
+    {
+        free_buf_pool.fetch(buf_p);
+        delete buf_p;
+    }
+
     delete mapper;
+
+
+    // Remove the partition-files.
+    for(std::size_t p_id = 0; p_id < partition_count; ++p_id)
+        if(std::remove(partition_file_path(p_id).c_str()))
+        {
+            std::cerr << "Error removing temporary files. Aborting.\n";
+            std::exit(EXIT_FAILURE);
+        }
 }
+
 
 template <typename T_key_, typename T_val_, typename T_hasher_>
 inline const std::string Key_Value_Collator<T_key_, T_val_, T_hasher_>::partition_file_path(const std::size_t partition_id) const
@@ -195,16 +238,15 @@ inline const std::string Key_Value_Collator<T_key_, T_val_, T_hasher_>::partitio
 template <typename T_key_, typename T_val_, typename T_hasher_>
 inline void Key_Value_Collator<T_key_, T_val_, T_hasher_>::map()
 {
-    std::vector<key_val_pair_t>* buf_ptr;
-    while(stream_incoming || consumed_buf < produced_buf)
-        if(buf_q.try_dequeue(buf_ptr))
+    buf_t* buf_p;
+
+    while(stream_incoming || !full_buf_pool.empty())
+        if(full_buf_pool.fetch(buf_p))
         {
-            map_buffer(*buf_ptr);
+            map_buffer(*buf_p);
 
-            buf_ptr->clear();
-            buf_q.enqueue(buf_ptr);
-
-            consumed_buf.fetch_add(1, std::memory_order_acquire);
+            buf_p->clear();
+            free_buf_pool.push(buf_p);
         }
 }
 
@@ -244,6 +286,32 @@ template <typename T_key_, typename T_val_, typename T_hasher_>
 inline std::size_t Key_Value_Collator<T_key_, T_val_, T_hasher_>::get_partition_id(const Key_Value_Collator::key_val_pair_t& key_val_pair) const
 {
     return hash(key_val_pair.first) & (partition_count - 1);
+}
+
+
+template <typename T_key_, typename T_val_, typename T_hasher_>
+inline void Key_Value_Collator<T_key_, T_val_, T_hasher_>::close_deposit_stream()
+{
+    stream_incoming = false;
+    if(!mapper->joinable())
+    {
+        std::cerr << "Early termination encountered for the key-mapper thread. Aborting.\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    mapper->join();
+
+
+    // Flush the remaining in-memory partition contents, release their memory, and close the in-disk partitions.
+    for(std::size_t p_id = 0; p_id < partition_count; ++p_id)
+    {
+        if(!partition_buf[p_id].empty())
+            flush(p_id);
+
+        buf_t().swap(partition_buf[p_id]);
+
+        partition_file[p_id].close();
+    }
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char* argv[])
     typedef std::size_t val_t;
     typedef key_value_collator::Identity_Functor<key_t> hasher_t;
     key_value_collator::Key_Value_Collator<key_t, val_t, hasher_t> kv_collator;
+    kv_collator.close_deposit_stream();
 
     return 0;
 }
