ec185532a18b25163aa0d885089a579a766b3976
jamshed <jamshed@umd.edu>
2022-10-25 21:18:48 -0400

    Add basic correctness check

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,78 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <vector>
+#include <set>
 #include <cstdlib>
 #include <thread>
 #include <random>
 #include <chrono>
+
+
+bool is_correct(const std::string& work_pref, const uint32_t thread_count)
+{
+    typedef uint32_t key_t;
+    typedef std::size_t val_t;
+    typedef key_value_collator::Identity_Functor<key_t> hasher_t;
+    typedef key_value_collator::Key_Value_Collator<key_t, val_t, hasher_t> kv_collator_t;
+    kv_collator_t kv_collator(work_pref, thread_count * 2);
+
+    std::vector<std::thread> worker;
+    worker.reserve(thread_count);
+    std::vector<std::vector<key_t>> v(thread_count);
+    for(uint32_t i = 0; i < thread_count; ++i)
+        worker.emplace_back(
+            [&kv_collator](std::vector<key_t>& keys)
+            {
+                constexpr uint32_t min = 0;
+                constexpr uint32_t max = std::numeric_limits<uint32_t>::max();
+
+                std::random_device rd;
+                std::mt19937 rng(rd());
+                std::uniform_int_distribution<uint32_t> uni(min, max);
+
+                constexpr std::size_t buf_sz = 10 * 1024 * 1024; // 10MB.
+                constexpr std::size_t buf_elem = buf_sz / sizeof(std::pair<key_t, val_t>);
+                for(uint32_t i = 0; i < 10; ++i)    // Each producer deposits 10 buffers.
+                {
+                    kv_collator_t::buf_t& buf = kv_collator.get_buffer();
+                    for(std::size_t j = 0; j < buf_elem; ++j)
+                        buf.emplace_back(uni(rd), uni(rd));
+
+                    std::for_each(buf.cbegin(), buf.cend(), [&keys](const auto& p){keys.emplace_back(p.first);});
+                    kv_collator.return_buffer(buf);
+                }
+            },
+            std::ref(v[i])
+        );
+
+    for(uint32_t i = 0; i < thread_count; ++i)
+        worker[i].join();
+
+    kv_collator.close_deposit_stream();
+
+    std::set<key_t> s;
+    std::for_each(v.cbegin(), v.cend(), [&s](const auto& vec)
+        { std::for_each(vec.cbegin(), vec.cend(), [&s](const auto p){ s.insert(p); }); });
+    std::cout << "Unique keys deposited: " << s.size() << "\n";
+
+    kv_collator.collate(thread_count);
+
+
+    kv_collator_t::iter_t it = kv_collator.begin();
+    const kv_collator_t::iter_t end = kv_collator.end();
+    std::vector<key_t> vec_it;
+    while(it != end)
+    {
+        vec_it.emplace_back(*it);
+        ++it;
+    }
+
+    std::cout << "Iterated over unique-key count: " << vec_it.size() << "\n";
+
+    std::sort(vec_it.begin(), vec_it.end());
+    return vec_it == std::vector<key_t>(s.cbegin(), s.cend());
+}
 
 
 int main(int argc, char* argv[])
@@ -64,6 +132,8 @@ int main(int argc, char* argv[])
     kv_collator.collate(thread_count);
     const auto t_2 = now();
     std::cout << "Collation done in " << duration(t_2 - t_1) << " seconds.\n";
+
+    std::cout << "Collated collection is " << (is_correct(work_pref, thread_count) ? "correct" : "incorrect") << "\n";
 
     return 0;
 }
